3772c83a0dfd89f3e97e3ddc9ca9f1126bf8426b
Denise Ichinco <ichinco@gmail.com>
2013-09-30 22:44:37 -0400

    a step toward setting up a rest service to get the segment change call

---
+++ src/main/scala/roadblock/lib/rest/RoadNetworkRest.scala
@@ -0,0 +1,29 @@
+package roadblock.lib.rest
+
+import net.liftweb._
+import common._
+import http._
+import rest._
+import util._
+import Helpers._
+import json._
+import scala.xml._
+import roadblock.lib.segments.SegmentState
+import roadblock.comet.Universe
+
+/**
+ * A full REST example
+ */
+object RoadNetworkRest extends RestHelper {
+
+  // Serve /api/item and friends
+  serve( "rest" / "network" prefix {
+
+    // POST if we find the item, merge the fields from the
+    // the POST body and update the item
+    case Nil JsonPost json -> _ =>
+      Universe ! "hello"
+
+
+  })
+}
