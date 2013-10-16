c5b2cc3faaf30dfd14cf892d719f2a46ca3b582b
Denise Ichinco <ichinco@gmail.com>
2013-10-15 21:35:54 -0400

    created an intersection

--- src/main/scala/roadblock/lib/segments/Intersection.scala
+++ src/main/scala/roadblock/lib/segments/Intersection.scala
@@ -1,6 +1,7 @@
 package roadblock.lib.segments
 
-import roadblock.lib.interfaces.{RoadState, RoadSegment}
+import roadblock.lib.interfaces.{Car, RoadState, RoadSegment}
+import scala.util.Random
 
 /**
  * User: denise
@@ -11,4 +12,20 @@ class Intersection extends NotNullSegment {
 
   var segmentType : Symbol= 'intersection
   var state : RoadState= null
+
+  override def acceptCar(car:Car) = {
+
+    val order : Seq[RoadSegment] = Random.shuffle(Seq(this.frontNeighbor, this.backNeighbor, this.leftSideNeighbor, this.rightSideNeighbor))
+    val newFront = order(0)
+    val newBack = order(1)
+    val newLeft = order(2)
+    val newRight = order(3)
+
+    this.frontNeighbor = newFront
+    this.backNeighbor = newBack
+    this.leftSideNeighbor = newLeft
+    this.rightSideNeighbor = newRight
+
+    super.acceptCar(car)
+  }
 }
